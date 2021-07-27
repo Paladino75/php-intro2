cf5f495df646e734d0b446adbde2863c25424e67
Paladino75 <yagodearaujo75pc@gmail.com>
2021-07-26 23:15:28 -0400

    exemplo para evoluir com Exceções

--- aula2/excecao.php
+++ aula2/excecao.php
@@ -1,5 +1,35 @@
 <?php
 
-	throw new Exception("Essa é uma exceção");
+	function validarUsuario(array $usuario)
+{
 
-	echo "\n... executando...\n";
+	if (empty($usuario['codigo']) || empty($usuario['nome']) || empty($usuario['idade'])){
+
+		return false;
+	}
+
+	return true;
+}
+
+$usuario = [
+
+		'codigo' => 1,
+		
+		'nome' => '',
+
+		'idade' => 57,
+];
+
+
+$usuariovalido = validarUsuario($usuario);
+
+
+if (!$usuariovalido){
+
+	echo "Usuário Inválido!".PHP_EOL;
+
+	return false;
+
+}
+
+echo "\n ... executando... \n".PHP_EOL;
